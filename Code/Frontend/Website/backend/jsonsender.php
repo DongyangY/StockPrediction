61d571de3341fb1deb9cacf806956a23862c2f60
DongyangY <dongyang.yao@rutgers.edu>
2016-06-17 13:21:09 -0400

    optimize jsonsender silly code

--- Code/Frontend/Website/backend/jsonsender.php
+++ Code/Frontend/Website/backend/jsonsender.php
@@ -18,34 +18,34 @@ $long_term_total = 20;
 $stock_name = array("GOOG", "YHOO", "AAPL", "FB", "MSFT", "AMZN", "SNE", "WMT", "CAJ", "TWTR");
 
 $sql = array("", "SELECT * FROM StockHistorical WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM LPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Position DESC LIMIT ".$long_term_total,
-				 "SELECT * FROM LPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Position DESC LIMIT ".$long_term_total,
-				 "SELECT * FROM PredictionMACD WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
-				 "SELECT * FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT 1",
-				 "SELECT * FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT 1",
-				 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 1'",
-				 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 2'",
-				 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 1'",
-				 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 2'",
-				 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."'",
-				 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."'",
-				 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%'",
-				 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%'");
+                 "SELECT * FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
+                 "SELECT * FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
+                 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%' ORDER BY Date DESC LIMIT ".$short_term_total,
+                 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%' ORDER BY Date DESC LIMIT ".$short_term_total,
+                 "SELECT * FROM LPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Position DESC LIMIT ".$long_term_total,
+                 "SELECT * FROM LPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Position DESC LIMIT ".$long_term_total,
+                 "SELECT * FROM PredictionMACD WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT ".$short_term_total,
+                 "SELECT * FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT 1",
+                 "SELECT * FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."' ORDER BY Date DESC LIMIT 1",
+                 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 1'",
+                 "SELECT * FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 2'",
+                 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 1'",
+                 "SELECT * FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date = 'predict day 2'",
+                 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM SPredictionBayesian WHERE Symbol = '".$stock_name[$id]."'",
+                 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM SPredictionSTS WHERE Symbol = '".$stock_name[$id]."'",
+                 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM PredictionSVM WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%'",
+                 "SELECT Symbol, AVG(RelativeDifference) AS avg FROM PredictionANN WHERE Symbol = '".$stock_name[$id]."' AND Date LIKE '%-%-%'");
 
 $result_arr = array();
 $temp_arr = array();
 
 for($i = 1; $i <= 18; $i++) {
-	if ($result = mysqli_query($con, $sql[$i])) {    
-  		while($row = $result->fetch_object()) {
-        	$temp_arr = $row;
-        	array_push($result_arr, $temp_arr);
-    	} 
-	}
+  if ($result = mysqli_query($con, $sql[$i])) {    
+    while($row = $result->fetch_object()) {
+      $temp_arr = $row;
+      array_push($result_arr, $temp_arr);
+    }
+  }
 }
 
 echo json_encode($result_arr);
